75cbb05c830d971a1bbe6b8d8d6d6a7ca7cc71f5
mar <martorrijos@protonmail.com>
2025-06-25 12:24:33 +0200

    feat: create GameService

---
+++ blackjack/src/main/java/org/blackjack/service/GameService.java
@@ -0,0 +1,39 @@
+package org.blackjack.service;
+
+import org.blackjack.model.Game;
+import org.blackjack.repositories.GameRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class GameService {
+
+    private final GameRepository gameRepository;
+
+    public GameService(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    public Mono<Game> addGame(Game game) {
+        return gameRepository.save(game);
+    }
+
+    public Mono<Game> getGame(int id) {
+        return gameRepository.findById(id);
+    }
+
+    public Mono<Game> updateGame(int id, Game game) {
+        return gameRepository.findById(id)
+                .flatMap(existingGame -> {
+                    game.setId(id);
+                    return gameRepository.save(game);
+                });
+    }
+
+    public Mono<Boolean> deleteGame(int id) {
+        return gameRepository.findById(id)
+                .flatMap(game -> gameRepository.deleteById(id).thenReturn(true))
+                .defaultIfEmpty(false);
+    }
+
+}

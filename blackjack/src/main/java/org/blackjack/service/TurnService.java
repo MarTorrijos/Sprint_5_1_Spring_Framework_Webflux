30f85185c2753e7b973d286c45e0e893f9501c1c
mar <martorrijos@protonmail.com>
2025-06-27 09:25:40 +0200

    fix and refactor code

--- blackjack/src/main/java/org/blackjack/service/TurnService.java
+++ blackjack/src/main/java/org/blackjack/service/TurnService.java
@@ -1,14 +1,10 @@
 package org.blackjack.service;
 
 import org.blackjack.model.Card;
-import org.blackjack.model.Deck;
 import org.blackjack.model.Game;
-import org.blackjack.model.Hand;
 import org.blackjack.model.enums.GameStatus;
 import org.blackjack.repositories.GameRepository;
 import reactor.core.publisher.Mono;
-
-import java.util.ArrayList;
 
 public class TurnService {
 
@@ -20,35 +16,31 @@ public class TurnService {
 
     public Mono<Game> dealCardToPlayer(String gameId) {
         return gameRepository.findById(gameId)
-                .flatMap(game -> {
-                    if (game.getGameStatus() == GameStatus.FINISHED) {
-                        return Mono.error(new IllegalStateException("Cannot deal card, game is finished"));
-                    }
+                .flatMap(this::validateGameStatus)
+                .flatMap(game -> drawCardFromDeck(game)
+                        .flatMap(newCard -> updatePlayerHand(newCard, game)))
+                .flatMap(gameRepository::save);
+    }
 
-                    if (game.getDeck() == null) {
-                        Deck deck = new Deck();
-                        game.setDeck(deck);
-                    }
+    private Mono<Game> validateGameStatus(Game game) {
+        if (game.getGameStatus() == GameStatus.FINISHED) {
+            return Mono.error(new IllegalStateException("Cannot deal card, game is finished"));
+        }
+        return Mono.just(game);
+    }
 
-                    Card newCard;
-                    try {
-                        newCard = game.getDeck().drawCard();
-                    } catch (IllegalStateException e) {
-                        return Mono.error(new RuntimeException("Deck is empty"));
-                    }
+    private Mono<Card> drawCardFromDeck(Game game) {
+        try {
+            Card newCard = game.getDeck().drawCard();
+            return Mono.just(newCard);
+        } catch (IllegalStateException e) {
+            return Mono.error(new RuntimeException("Deck is empty"));
+        }
+    }
 
-                    if (game.getPlayerHand() == null) {
-                        game.setPlayerHand(new Hand());
-                    }
-
-                    if (game.getPlayerHand().getCards() == null) {
-                        game.getPlayerHand().setCards(new ArrayList<>());
-                    }
-
-                    game.getPlayerHand().getCards().add(newCard);
-
-                    return gameRepository.save(game);
-                });
+    private Mono<Game> updatePlayerHand(Card newCard, Game game) {
+        game.getPlayerHand().getCards().add(newCard);
+        return Mono.just(game);
     }
 
     public Mono<Boolean> playerStands(String gameId) {

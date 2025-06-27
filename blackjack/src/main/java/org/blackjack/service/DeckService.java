f8cac116578497157760c4c412dbe9419aa13970
mar <martorrijos@protonmail.com>
2025-06-27 11:44:28 +0200

    refactor: some TurnService methods to DeckService

---
+++ blackjack/src/main/java/org/blackjack/service/DeckService.java
@@ -0,0 +1,32 @@
+package org.blackjack.service;
+
+import org.blackjack.model.Card;
+import org.blackjack.model.Deck;
+import org.blackjack.model.Game;
+import org.blackjack.validations.DeckValidator;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class DeckService {
+
+    private final DeckValidator deckValidator;
+
+    public DeckService(DeckValidator deckValidator) {
+        this.deckValidator = deckValidator;
+    }
+
+    public Mono<Game> initializeDeck(Game game) {
+        if (game.getDeck() == null) {
+            game.setDeck(new Deck());
+            game.getDeck().reset();
+        }
+        return Mono.just(game);
+    }
+
+    public Mono<Card> drawCard(Game game) {
+        return deckValidator.validateDeck(game.getDeck())
+                .then(Mono.fromCallable(() -> game.getDeck().drawCard()));
+    }
+
+}
--- blackjack/src/main/java/org/blackjack/service/TurnService.java
+++ blackjack/src/main/java/org/blackjack/service/TurnService.java
@@ -1,11 +1,9 @@
 package org.blackjack.service;
 
 import org.blackjack.model.Card;
-import org.blackjack.model.Deck;
 import org.blackjack.model.Game;
 import org.blackjack.model.enums.GameStatus;
 import org.blackjack.repositories.GameRepository;
-import org.blackjack.validations.DeckValidator;
 import org.blackjack.validations.GameValidator;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -15,38 +13,25 @@ public class TurnService {
 
     private final GameRepository gameRepository;
     private final GameValidator gameValidator;
-    private final DeckValidator deckValidator;
+    private final DeckService deckService;
 
-    public TurnService(GameRepository gameRepository, GameValidator gameValidator, DeckValidator deckValidator) {
+    public TurnService(GameRepository gameRepository, GameValidator gameValidator, DeckService deckService) {
         this.gameRepository = gameRepository;
         this.gameValidator = gameValidator;
-        this.deckValidator = deckValidator;
+        this.deckService = deckService;
     }
 
     public Mono<Game> dealCardToPlayer(String gameId) {
         return gameRepository.findById(gameId)
                 .flatMap(gameValidator::validateGameStatus)
-                .flatMap(this::initializeDeckIfNeeded)
-                .flatMap(game -> drawCardFromDeck(game)
+                .flatMap(deckService::initializeDeck)
+                .flatMap(game -> deckService.drawCard(game)
                         .flatMap(newCard -> updatePlayerHand(newCard, game)))
                 .flatMap(gameRepository::save);
     }
 
-    private Mono<Card> drawCardFromDeck(Game game) {
-        return deckValidator.validateDeck(game.getDeck())
-                .then(Mono.fromCallable(() -> game.getDeck().drawCard()));
-    }
-
     private Mono<Game> updatePlayerHand(Card newCard, Game game) {
         game.getPlayerHand().getCards().add(newCard);
-        return Mono.just(game);
-    }
-
-    public Mono<Game> initializeDeckIfNeeded(Game game) {
-        if (game.getDeck() == null) {
-            game.setDeck(new Deck());
-            game.getDeck().reset();
-        }
         return Mono.just(game);
     }
 
